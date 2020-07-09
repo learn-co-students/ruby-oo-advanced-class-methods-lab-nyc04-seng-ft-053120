9413946bd9cb28f868ba06d6eb8d5a1d06a6250c
chrisvsanchez <chrisvsanchez@gmail.com>
2020-07-09 13:15:17 -0400

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,5 +1,7 @@
+require 'pry'
 class Song
   attr_accessor :name, :artist_name
+
   @@all = []
 
   def self.all
@@ -8,6 +10,62 @@ class Song
 
   def save
     self.class.all << self
+  end
+
+  def self.create 
+      @@all << self.new
+      return @@all[-1]
+  end
+
+  def self.new_by_name(name)
+      new_song = self.new 
+      new_song.name = name
+      new_song
+  end
+  def self.create_by_name(name)
+    #instantiate song varible create new instance
+    song = self.new 
+    #access song and assign name parameter
+    song.name = name
+    #store the object song into the array @@all Class variable scope of entire class. 
+    self.all << song
+    song
+  end
+def self.find_by_name(name)
+  @@all.find{|song|song.name == name}
+  # binding.pry
+end
+
+def self.find_or_create_by_name(name)
+  song = self.find_by_name(name)
+  if song == nil
+    song = create_by_name(name)
+  end
+  song 
+  end
+def self.alphabetical 
+  self.all.sort_by{|song| song.name}
+  # binding.pry
+end
+
+def self.new_from_filename(file)
+ arr = file.split(" - ")
+ #=>> 
+ arr[1].slice!(".mp3")
+  # binding.pry
+ song = Song.new_by_name(arr[1])
+ song.artist_name = arr[0]
+ song
+end
+
+def self.create_from_filename(file)
+  song = self.new_from_filename(file)
+  @@all << song
+end
+
+
+  def self.destroy_all
+      @@all = []
   end
 
 end
